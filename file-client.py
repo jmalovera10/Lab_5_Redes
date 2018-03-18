9b059873e109b2c66ae9a0b9dbd8aa7ffbfda359
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-03-18 15:42:18 -0500

    Server modified

---
+++ file-client.py
@@ -0,0 +1,31 @@
+import socket
+import sys
+from datetime import datetime
+
+if __name__ == "__main__":
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    server_address = (sys.argv[1], int(sys.argv[2]))
+    object_amount = int(sys.argv[3])
+    try:
+        # Send data
+        for i in range(object_amount):
+            message = {
+                "sequence": i+1,
+                "sequenceLength": object_amount,
+                "sentTime": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+            }
+
+            print(message["sentTime"])
+
+            print(sys.stderr, 'sending "%s"' % message)
+            sent = sock.sendto(str(message).encode('utf-8'), server_address)
+
+            # Receive response
+            #print(sys.stderr, 'waiting to receive')
+            # data, server = sock.recvfrom(4096)
+            #print(sys.stderr, 'received "%s"' % data)
+
+    finally:
+        print(sys.stderr, 'closing socket')
+        sock.close()
