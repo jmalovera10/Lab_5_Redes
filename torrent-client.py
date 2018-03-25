38557f58ad1f47dcd76ae26f4f6cc27f82b02a74
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-03-25 10:52:19 -0500

    Tomas lok

--- torrent-client.py
+++ torrent-client.py
@@ -1,4 +1,21 @@
+from utorrentapi import UTorrentAPI
 import sys
+import json
 
 if __name__ == "__main__":
-# Arg1 IP, Arg2 Port, Arg3 Username, Arg4 Password
+    # Arg1 Url, Arg3 Username, Arg4 Password
+    client = UTorrentAPI(sys.argv[1], sys.argv[2], sys.argv[3])
+    data = client.get_list()
+    print(data)
+
+    # Get torrent names
+    torrents = {}
+    for d in data["torrents"]:
+        torrents[d[2]] = {
+            "id": d[0]
+        }
+    print("Available torrents: " + json.dumps(torrents, indent=2))
+    file = raw_input("Select a torrent>> ")
+    get_torrent = torrents[file]
+    get = client.get_files(get_torrent["id"])
+    print(get)
